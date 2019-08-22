1c41904a5e6f0cda3fede384b399305b214748d0
Emin <allen.emin@gmail.com>
2019-08-22 00:11:53 -0400

    updated menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/homepage.component.jsx";
 
 class App extends React.Component {
   render() {
-
     return (
       <div className="App">
         <Homepage />
       </div>
     );
   }
-
 }
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import MenuItem from "../menu-item/menu-item.component";
+
 import "./directory.styles.scss";
 
 class Directory extends Component {
@@ -13,29 +14,34 @@ class Directory extends Component {
           imageUrl:
             "https://www.galtandmaree.com/uploads/5/3/5/2/53527881/60197415-2110880252545346-1828218357858435072-o_orig.jpg",
           size: "large",
-          id: 1
+          id: 1,
+          linkUrl: 'candles'
         },
         {
           title: "diffusers",
           imageUrl: "https://www.galtandmaree.com/uploads/5/3/5/2/53527881/fullsizeoutput-25b9_orig.jpeg",
           size: "large",
-          id: 2
+          id: 2,
+          linkUrl: 'diffusers'
         },
         {
           title: "flameless",
           imageUrl: "https://www.westelm.com/weimgs/ab/images/wcm/products/201917/0014/premium-flameless-candles-c.jpg",
           size: 'large',
-          id: 3
+          id: 3,
+          linkUrl: 'flameless'
         },
         {
           title: "accessories",
           imageUrl: "https://tetontimberlinetrading.com/wp-content/uploads/2015/05/LED-Pillars-Ivory-Deco-799x450.jpg",
-          id: 5
+          id: 5,
+          linkUrl: 'accessories'
         },
         {
           title: "gifts",
           imageUrl: "https://www.galtandmaree.com/uploads/5/3/5/2/53527881/img-8497_orig.jpg",
-          id: 6
+          id: 6,
+          linkUrl: 'gifts'
         }
       ]
     };
@@ -44,8 +50,8 @@ class Directory extends Component {
     const { sections } = this.state;
     return (
       <div className="directory-menu">
-        {sections.map(({ size, title, imageUrl, id }) => (
-          <MenuItem size={size} imageUrl={imageUrl} key={id} id={id} title={title} />
+        {sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem  key={id} id={id} {...otherSectionProps} />
         ))}
       </div>
     );
